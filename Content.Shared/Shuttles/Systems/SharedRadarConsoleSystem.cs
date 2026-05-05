44522989711720952a70149c45f16d8463d9696f
Aserovich creator <aser.ionov15@gmail.com>
2026-05-05 13:05:51 +0300

    i forgot about comments

--- Content.Shared/Shuttles/Systems/SharedRadarConsoleSystem.cs
+++ Content.Shared/Shuttles/Systems/SharedRadarConsoleSystem.cs
@@ -4,7 +4,7 @@ namespace Content.Shared.Shuttles.Systems;
 
 public abstract class SharedRadarConsoleSystem : EntitySystem
 {
-    public const float DefaultMaxRange = 256f;
+    // public const float DefaultMaxRange = 256f; // OH14-Changes
 
     protected virtual void UpdateState(EntityUid uid, RadarConsoleComponent component)
     {
