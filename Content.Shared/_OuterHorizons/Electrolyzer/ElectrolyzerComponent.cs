0b5d58457910898ebd2c187206e0a857f240fa77
ANT-14THEFIRST <anewtwinktwink@gmail.com>
2026-08-06 18:11:39 +0300

    Update ElectrolyzerComponent.cs

--- Content.Shared/_OuterHorizons/Electrolyzer/ElectrolyzerComponent.cs
+++ Content.Shared/_OuterHorizons/Electrolyzer/ElectrolyzerComponent.cs
@@ -14,27 +14,15 @@ public sealed partial class ElectrolyzerComponent : Component
     [DataField("releasedGases")]
     public Dictionary<Gas, FixedPoint2> ReleasedGases = new() { { Gas.Oxygen, 1/3f }, { Gas.Tritium, 2/3f } };
 
-    /// <summary>
-    /// Выходная труба для кислорода.
-    /// </summary>
     [DataField]
     public ProtoId<ReagentPrototype> LiquidToConsume = "Water";
 
-    /// <summary>
-    /// Выходная труба.
-    /// </summary>
     [DataField]
     public string Outlet = "outlet";
 
-    /// <summary>
-    /// Имя solution, откуда берётся жидкость (заполняется сторонней системой водопроводов).
-    /// </summary>
     [DataField]
     public string SolutionName = "tank";
 
-    /// <summary>
-    /// Сколько единиц жидкости (u) расходуем за секунду.
-    /// </summary>
     [DataField]
     public float LiquidConsumptionRate = 5f;
 }
