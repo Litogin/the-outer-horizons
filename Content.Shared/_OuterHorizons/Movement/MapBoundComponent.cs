1c0b38dcaa8ad874e2ccbe29373e98b8ea9ee732
Aserovich creator <aser.ionov15@gmail.com>
2026-08-25 10:50:27 +0300

    its here

---
+++ Content.Shared/_OuterHorizons/Movement/MapBoundComponent.cs
@@ -0,0 +1,13 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._OuterHorizons.Movement;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(fieldDeltas: true)]
+public sealed partial class MapBoundsComponent : Component
+{
+    [ViewVariables(VVAccess.ReadWrite), DataField, AutoNetworkedField]
+    public float Radius { get; set; } = 4000f;
+
+    [ViewVariables(VVAccess.ReadWrite), DataField, AutoNetworkedField]
+    public float BaseImpulseVelocity { get; set; } = 1f;
+}
